aa6296abad6e5121c9940c58e565ece11dac5990
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-02-10 12:31:46 +0100

    create container process

--- unified-managed-deploy/src/main/java/it/redhat/demo/customtask/CreateContainerSpec.java
+++ unified-managed-deploy/src/main/java/it/redhat/demo/customtask/CreateContainerSpec.java
@@ -1,6 +1,8 @@
 package it.redhat.demo.customtask;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
@@ -10,19 +12,21 @@ import org.kie.server.api.model.ReleaseId;
 import org.kie.server.controller.api.model.spec.Capability;
 import org.kie.server.controller.api.model.spec.ContainerSpec;
 import org.kie.server.controller.api.model.spec.ProcessConfig;
+import org.kie.server.controller.api.model.spec.ServerTemplate;
 import org.kie.server.controller.api.model.spec.ServerTemplateKey;
 
 public class CreateContainerSpec implements WorkItemHandler {
 
 	@Override
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		ServerTemplate serverTemplate = (ServerTemplate) workItem.getParameter("serverTemplate");
 		String groupId = (String) workItem.getParameter("groupId");
 		String artifactId = (String) workItem.getParameter("artifactId");
 		String version = (String) workItem.getParameter("version");
 		String serverId = (String) workItem.getParameter("serverId");
 		String gav = groupId + ":" + artifactId + ":" + version;
 		
-		ServerTemplateKey serverTemplate = new ServerTemplateKey(serverId, serverId);
+		ServerTemplateKey serverTemplateKey = new ServerTemplateKey(serverId, serverId);
 		ReleaseId releaseId = new ReleaseId(groupId, artifactId, version);
 		ProcessConfig processConfig = new ProcessConfig();
 		processConfig.setRuntimeStrategy("PER_PROCESS_INSTANCE");
@@ -31,13 +35,16 @@ public class CreateContainerSpec implements WorkItemHandler {
 		ContainerSpec container = new ContainerSpec();
 		container.setId(gav);
 		container.setContainerName(gav);
-		container.setServerTemplateKey(serverTemplate);
+		container.setServerTemplateKey(serverTemplateKey);
 		container.setReleasedId(releaseId);
 		container.setStatus(KieContainerStatus.STARTED);
 		container.addConfig(Capability.PROCESS, processConfig);
 		
+		List<String> processServerUrls = serverTemplate.getServerInstanceKeys().stream().map(key -> key.getUrl()).collect(Collectors.toList());
+		
 		HashMap<String,Object> results = new HashMap<>();
 		results.put("psDeployRequest", container);
+		results.put("processServerUrls", processServerUrls);
 		
 	}
 
