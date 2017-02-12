5c1d3cc2a7645e4d37d7a6e85844a4a1ff04beab
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-02-12 09:31:54 +0100

    unified managed deploy unit test

---
+++ unified-managed-deploy/src/test/java/it/redhat/test/stub/BusinessCentralDeployStub.java
@@ -0,0 +1,19 @@
+package it.redhat.test.stub;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class BusinessCentralDeployStub implements WorkItemHandler {
+
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		
+	}
+
+}
---
+++ unified-managed-deploy/src/test/java/it/redhat/test/stub/ProcessServerDeployStub.java
@@ -0,0 +1,21 @@
+package it.redhat.test.stub;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class ProcessServerDeployStub implements WorkItemHandler {
+
+	@Override
+	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
