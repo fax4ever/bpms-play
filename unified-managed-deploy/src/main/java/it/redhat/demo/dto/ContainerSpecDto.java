bbf21291c6074696fba6e80c0fd446bddb8a076b
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-02-12 15:39:59 +0100

    serizable dtos

---
+++ unified-managed-deploy/src/main/java/it/redhat/demo/dto/ContainerSpecDto.java
@@ -0,0 +1,12 @@
+package it.redhat.demo.dto;
+
+import java.io.Serializable;
+
+public class ContainerSpecDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1504778524501872681L;
+
+}
---
+++ unified-managed-deploy/src/main/java/it/redhat/demo/dto/ServerTemplateDto.java
@@ -0,0 +1,18 @@
+package it.redhat.demo.dto;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "server-template-details")
+public class ServerTemplateDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3861051750508032256L;
+
+}
