f5c1070615bafb74f4cf9b8b817d4d4683a6e3b1
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-03-01 14:52:33 +0100

    async execution test

--- bpms-asynchronous-execution/src/test/java/it/redhat/demo/AsyncExecutionProcessTest.java
+++ bpms-asynchronous-execution/src/test/java/it/redhat/demo/AsyncExecutionProcessTest.java
@@ -1,5 +1,7 @@
 package it.redhat.demo;
 
+import java.util.HashMap;
+
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -7,6 +9,10 @@ import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
 
 public class AsyncExecutionProcessTest extends JbpmJUnitBaseTestCase {
 	
@@ -27,6 +33,22 @@ public class AsyncExecutionProcessTest extends JbpmJUnitBaseTestCase {
 
 		runtimeEngine = getRuntimeEngine();
 		kieSession = runtimeEngine.getKieSession();
+		
+		// we need to stub the async executor wid
+		// because the test runtime don't see jms subsystem
+		kieSession.getWorkItemManager().registerWorkItemHandler("AsyncCommand", new WorkItemHandler() {
+			
+			@Override
+			public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+				workItemManager.completeWorkItem(workItem.getId(), new HashMap<>());
+			}
+			
+			@Override
+			public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+				
+			}
+			
+		});
 
 	}
 	
@@ -40,6 +62,11 @@ public class AsyncExecutionProcessTest extends JbpmJUnitBaseTestCase {
 	
 	@Test
 	public void test() {
+		
+		ProcessInstance pi = kieSession.startProcess("it.redhat.demo.async-execution-process");
+		
+		// because the stub is sync
+		assertProcessInstanceCompleted(pi.getId());
 		
 	}
 
