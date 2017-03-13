907df32c4588e48d3baeebf18d3728ef75a32345
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-03-13 16:02:26 +0100

    correlation key on subprocess test

--- bpms-correlation-key/src/main/java/it/redhat/demo/listener/MyProcessEventListener.java
+++ bpms-correlation-key/src/main/java/it/redhat/demo/listener/MyProcessEventListener.java
@@ -1,8 +1,10 @@
 package it.redhat.demo.listener;
 
+import org.jbpm.process.instance.impl.ProcessInstanceImpl;
 import org.kie.api.event.process.DefaultProcessEventListener;
 import org.kie.api.event.process.ProcessVariableChangedEvent;
-import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.runtime.KieRuntime;
+import org.kie.internal.process.CorrelationKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,11 +17,31 @@ public class MyProcessEventListener extends DefaultProcessEventListener {
 		
 		Object newValue = event.getNewValue();
 		Object oldValue = event.getOldValue();
-		ProcessInstance processInstance = event.getProcessInstance();
 		String variableId = event.getVariableId();
 		String variableInstanceId = event.getVariableInstanceId();
 		
-		log.info("variableId {}, variableInstanceId {}, oldValue {}, newValue {}, processInstance {}", variableId, variableInstanceId, oldValue, newValue, processInstance);
+		ProcessInstanceImpl currentPi = (ProcessInstanceImpl) event.getProcessInstance();
+		ProcessInstanceImpl rootPi = MyProcessEventListener.findRoot(event.getKieRuntime(), currentPi);
+		
+		CorrelationKey correlationKey = (CorrelationKey) rootPi.getMetaData().get("CorrelationKey");
+		String corrleationKeyString = "No correlation key";
+		if (correlationKey != null) {
+			corrleationKeyString = correlationKey.toExternalForm();
+		}
+		
+		log.info("correlationKey {}, variableId {}, variableInstanceId {}, oldValue {}, newValue {}, processInstance {}, rootProcessInstance {}", corrleationKeyString, variableId, variableInstanceId, oldValue, newValue, currentPi.getId(), rootPi.getId());
+		
+	}
+	
+	public static ProcessInstanceImpl findRoot(KieRuntime kruntime, ProcessInstanceImpl pi) {
+		
+		long parentProcessInstanceId = pi.getParentProcessInstanceId();
+		if (parentProcessInstanceId <= 0) {
+			return pi;
+		}
+		
+		ProcessInstanceImpl parentProcessInstance = (ProcessInstanceImpl) kruntime.getProcessInstance(parentProcessInstanceId, true);
+		return MyProcessEventListener.findRoot(kruntime, parentProcessInstance);
 		
 	}
 
