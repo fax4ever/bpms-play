27afa01e9d831d18087c04a1d966dcf3aa2dfd93
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-03-15 16:33:36 +0100

    adding start / end process logging (always with correlation key)

--- bpms-correlation-key/src/main/java/it/redhat/demo/listener/LogTaskEventListener.java
+++ bpms-correlation-key/src/main/java/it/redhat/demo/listener/LogTaskEventListener.java
@@ -2,161 +2,43 @@ package it.redhat.demo.listener;
 
 import org.jbpm.services.task.events.DefaultTaskEventListener;
 import org.kie.api.task.TaskEvent;
+import org.kie.api.task.model.Task;
+import org.kie.api.task.model.TaskData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class LogTaskEventListener extends DefaultTaskEventListener {
 	
 	private static final Logger logger = LoggerFactory.getLogger(LogTaskEventListener.class);
 
 	@Override
-	public void beforeTaskStoppedEvent(TaskEvent event) {
-		logger.info("beforeTaskStoppedEvent");
-	}
-
-	@Override
-	public void beforeTaskExitedEvent(TaskEvent event) {
-		logger.info("beforeTaskExitedEvent");
-	}
-
-	@Override
-	public void afterTaskStartedEvent(TaskEvent event) {
-		logger.info("afterTaskStartedEvent");
-	}
-
-	@Override
-	public void afterTaskStoppedEvent(TaskEvent event) {
-		logger.info("afterTaskStoppedEvent");
-	}
-
-	@Override
-	public void afterTaskAddedEvent(TaskEvent event) {
-		logger.info("afterTaskAddedEvent");
-	}
-
-	@Override
-	public void beforeTaskActivatedEvent(TaskEvent event) {
-		logger.info("beforeTaskActivatedEvent");
-	}
-
-	@Override
-	public void beforeTaskClaimedEvent(TaskEvent event) {
-		logger.info("beforeTaskClaimedEvent");
-	}
-
-	@Override
-	public void beforeTaskSkippedEvent(TaskEvent event) {
-		logger.info("beforeTaskSkippedEvent");
-	}
-
-	@Override
-	public void beforeTaskStartedEvent(TaskEvent event) {
-		logger.info("beforeTaskStartedEvent");
-	}
-
-	@Override
-	public void beforeTaskCompletedEvent(TaskEvent event) {
-		logger.info("beforeTaskCompletedEvent");
-	}
-
-	@Override
-	public void beforeTaskFailedEvent(TaskEvent event) {
-		logger.info("beforeTaskFailedEvent");
-	}
-
-	@Override
-	public void beforeTaskAddedEvent(TaskEvent event) {
-		logger.info("beforeTaskAddedEvent");
-	}
-
-	@Override
-	public void beforeTaskReleasedEvent(TaskEvent event) {
-		logger.info("beforeTaskReleasedEvent");
-	}
-
-	@Override
-	public void beforeTaskResumedEvent(TaskEvent event) {
-		logger.info("beforeTaskResumedEvent");
-	}
-
-	@Override
-	public void beforeTaskSuspendedEvent(TaskEvent event) {
-		logger.info("beforeTaskSuspendedEvent");
-	}
-
-	@Override
-	public void beforeTaskForwardedEvent(TaskEvent event) {
-		logger.info("beforeTaskForwardedEvent");
-	}
-
-	@Override
-	public void beforeTaskDelegatedEvent(TaskEvent event) {
-		logger.info("beforeTaskDelegatedEvent");
-	}
-
-	@Override
-	public void beforeTaskNominatedEvent(TaskEvent event) {
-		logger.info("beforeTaskNominatedEvent");
-	}
-
-	@Override
-	public void afterTaskActivatedEvent(TaskEvent event) {
-		logger.info("afterTaskActivatedEvent");
-	}
-
-	@Override
 	public void afterTaskClaimedEvent(TaskEvent event) {
-		logger.info("afterTaskClaimedEvent");
-	}
+		Task task = event.getTask();
+		TaskData taskData = task.getTaskData();
 
-	@Override
-	public void afterTaskSkippedEvent(TaskEvent event) {
-		logger.info("afterTaskClaimedEvent");
+		logger.info("Task Claimed - taskName: [{}], processId: [{}], pi: [{}]",
+				task.getName(), taskData.getProcessId(), taskData.getProcessInstanceId());
 	}
 
 	@Override
 	public void afterTaskCompletedEvent(TaskEvent event) {
-		logger.info("afterTaskCompletedEvent");
-	}
 
-	@Override
-	public void afterTaskFailedEvent(TaskEvent event) {
-		logger.info("afterTaskFailedEvent");
-	}
+		Task task = event.getTask();
 
-	@Override
-	public void afterTaskExitedEvent(TaskEvent event) {
-		logger.info("afterTaskExitedEvent");
-	}
+		task = event.getTaskContext().loadTaskVariables(task);
+		TaskData taskData = task.getTaskData();
 
-	@Override
-	public void afterTaskReleasedEvent(TaskEvent event) {
-		logger.info("afterTaskReleasedEvent");
-	}
+		long processInstanceId = taskData.getProcessInstanceId();
+		String processId = taskData.getProcessId();
 
-	@Override
-	public void afterTaskResumedEvent(TaskEvent event) {
-		logger.info("afterTaskResumedEvent");
-	}
+		Map<String, Object> taskInputVariables = taskData.getTaskInputVariables();
+		Map<String, Object> taskOutputVariables = taskData.getTaskOutputVariables();
 
-	@Override
-	public void afterTaskSuspendedEvent(TaskEvent event) {
-		logger.info("afterTaskSuspendedEvent");
-	}
+		logger.info("Task Completed - taskName: [{}], processId: [{}], pi: [{}], in: [{}], out: pi: [{}]",
+				task.getName(), processId, processInstanceId, taskInputVariables, taskOutputVariables);
 
-	@Override
-	public void afterTaskForwardedEvent(TaskEvent event) {
-		logger.info("afterTaskForwardedEvent");
-	}
-
-	@Override
-	public void afterTaskDelegatedEvent(TaskEvent event) {
-		logger.info("afterTaskDelegatedEvent");
-	}
-
-	@Override
-	public void afterTaskNominatedEvent(TaskEvent event) {
-		logger.info("afterTaskNominatedEvent");
 	}
 	
 }
