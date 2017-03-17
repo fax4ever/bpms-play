3ad65df719da47980ba4fe17874b5e2946fbe4fd
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-03-17 13:25:23 +0100

    new project for exploring the advanced queries

---
+++ bpms-advanced-query/src/test/java/it/redhat/demo/AdvancedQueryTest.java
@@ -0,0 +1,13 @@
+package it.redhat.demo;
+
+import org.jbpm.test.JbpmJUnitBaseTestCase;
+
+/**
+ * Created by fabio.ercoli@redhat.com on 17/03/17.
+ */
+public class AdvancedQueryTest extends JbpmJUnitBaseTestCase {
+
+
+
+
+}
