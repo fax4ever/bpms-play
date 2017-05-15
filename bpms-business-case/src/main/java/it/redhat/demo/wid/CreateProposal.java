2c5d95996e2c1453daed463659ea2fc4c14dbd7b
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-05-15 10:23:10 +0200

    proposal

--- bpms-business-case/src/main/java/it/redhat/demo/wid/CreateProposal.java
+++ bpms-business-case/src/main/java/it/redhat/demo/wid/CreateProposal.java
@@ -1,5 +1,6 @@
 package it.redhat.demo.wid;
 
+import java.util.Date;
 import java.util.HashMap;
 
 import org.kie.api.runtime.process.WorkItem;
@@ -7,18 +8,32 @@ import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 
 import it.redhat.demo.entity.ProposalEntity;
+import it.redhat.demo.entity.SubjectEntity;
 
 public class CreateProposal implements WorkItemHandler {
 
 	@Override
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
 		
-		HashMap<Object,Object> results = new HashMap<>();
+		HashMap<String,Object> results = new HashMap<>();
 		
 		ProposalEntity proposal = new ProposalEntity();
+        proposal.setStart(new Date());
+        proposal.setAmount(731);
+        proposal.setAcquire(new Date());
+
+        SubjectEntity subject = new SubjectEntity();
+        subject.setTaxcode("237832788327sddss23");
+        subject.setSurname("Ercoli");
+        subject.setName("Fabio Masimo");
+        subject.setBirth(new Date());
+
+        proposal.setSubject(subject);
 		
+		results.put("proposal", proposal);
 		
-		results.put("proposal", new ProposalEntity());
+		manager.completeWorkItem(workItem.getId(), results);
+		
 		
 	}
 
