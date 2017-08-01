b392aab61812db0905a756d109e274eb6f891ba8
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-08-01 11:58:47 +0200

    using Administrator user to delegate task

--- bpms-business-admin/src/test/java/it/redhat/demo/process/BusinessAdminProcessTest.java
+++ bpms-business-admin/src/test/java/it/redhat/demo/process/BusinessAdminProcessTest.java
@@ -16,9 +16,16 @@ import org.kie.api.runtime.manager.audit.AuditService;
 import org.kie.api.runtime.manager.audit.VariableInstanceLog;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.task.TaskService;
+import org.kie.api.task.model.Status;
 import org.kie.api.task.model.TaskSummary;
 
 public class BusinessAdminProcessTest extends JbpmJUnitBaseTestCase {
+
+	private static final String USER_HR1 = "mgallerani";
+
+	private static final String USER_HR2 = "pgarzone";
+
+	private static final String USER_ADMINISTRATOR = "Administrator";
 
 	private static final String THE_MANAGER_FEEDBACK = "the Manager feedback!";
 
@@ -70,45 +77,44 @@ public class BusinessAdminProcessTest extends JbpmJUnitBaseTestCase {
 		// HR user do task
 		parameters.clear();
 		parameters.put("feed", THE_HR_FEEDBACK);
-		doTask("pgarzone", parameters);
-		
+		doTask(USER_HR2, parameters);
+
 		assertProcessInstanceActive(pi.getId());
 		assertNodeTriggered(pi.getId(), "Manager Task");
 
-		// HR user do task
+		// Manager user do task
 		parameters.clear();
 		parameters.put("feed", THE_MANAGER_FEEDBACK);
 		doTask("gpirola", parameters);
-		
+
 		assertProcessInstanceCompleted(pi.getId());
 		assertNodeTriggered(pi.getId(), "End Process");
-		
-		Map<String, List<VariableInstanceLog>> variables = auditService.findVariableInstances(pi.getId())
-			.stream()
-			.collect(Collectors.groupingBy(VariableInstanceLog::getVariableId));
-		
+
+		Map<String, List<VariableInstanceLog>> variables = auditService.findVariableInstances(pi.getId()).stream()
+				.collect(Collectors.groupingBy(VariableInstanceLog::getVariableId));
+
 		List<VariableInstanceLog> startLogs = variables.get("start");
 		assertEquals(1, startLogs.size());
 		VariableInstanceLog startLog = startLogs.get(0);
-		
+
 		assertEquals(START_VALUE, startLog.getValue());
-	
+
 		List<VariableInstanceLog> hrLogs = variables.get("hr");
 		assertEquals(1, hrLogs.size());
 		VariableInstanceLog hrLog = hrLogs.get(0);
-		
+
 		assertEquals(THE_HR_FEEDBACK, hrLog.getValue());
-		
+
 		List<VariableInstanceLog> managerLogs = variables.get("manager");
 		assertEquals(1, managerLogs.size());
 		VariableInstanceLog managerLog = managerLogs.get(0);
-		
+
 		assertEquals(THE_MANAGER_FEEDBACK, managerLog.getValue());
 
 	}
 
 	private void doTask(String user, HashMap<String, Object> parameters) {
-		
+
 		List<TaskSummary> tasks = taskService.getTasksAssignedAsPotentialOwner(user, "");
 		assertEquals(1, tasks.size());
 		TaskSummary task = tasks.get(0);
@@ -116,7 +122,58 @@ public class BusinessAdminProcessTest extends JbpmJUnitBaseTestCase {
 		taskService.claim(task.getId(), user);
 		taskService.start(task.getId(), user);
 		taskService.complete(task.getId(), user, parameters);
-		
+
+	}
+
+	@Test
+	public void test_business_admin() {
+
+		HashMap<String, Object> parameters = new HashMap<>();
+		parameters.put("start", START_VALUE);
+
+		ProcessInstance pi = kieSession.startProcess("it.redhat.demo.business-admin", parameters);
+		assertProcessInstanceActive(pi.getId());
+		assertNodeTriggered(pi.getId(), "Start Process", "HR Task");
+
+		List<TaskSummary> tasks = taskService.getTasksAssignedAsBusinessAdministrator(USER_ADMINISTRATOR, "");
+		assertEquals(1, tasks.size());
+		TaskSummary task = tasks.get(0);
+		assertEquals(Status.Ready, task.getStatus());
+
+		taskService.claim(task.getId(), USER_HR1);
+
+		tasks = taskService.getTasksAssignedAsBusinessAdministrator(USER_ADMINISTRATOR, "");
+		assertEquals(1, tasks.size());
+		task = tasks.get(0);
+		assertEquals(Status.Reserved, task.getStatus());
+		assertEquals(USER_HR1, task.getActualOwnerId());
+
+		taskService.delegate(task.getId(), USER_ADMINISTRATOR, USER_HR2);
+
+		tasks = taskService.getTasksAssignedAsBusinessAdministrator(USER_ADMINISTRATOR, "");
+		assertEquals(1, tasks.size());
+		task = tasks.get(0);
+		assertEquals(Status.Reserved, task.getStatus());
+		assertEquals(USER_HR2, task.getActualOwnerId());
+
+		taskService.start(task.getId(), USER_HR2);
+
+		parameters.clear();
+		parameters.put("feed", "This is Garzone feedback!");
+
+		taskService.complete(task.getId(), USER_HR2, parameters);
+
+		assertProcessInstanceActive(pi.getId());
+		assertNodeTriggered(pi.getId(), "Manager Task");
+
+		// Manager user do task
+		parameters.clear();
+		parameters.put("feed", THE_MANAGER_FEEDBACK);
+		doTask("gpirola", parameters);
+
+		assertProcessInstanceCompleted(pi.getId());
+		assertNodeTriggered(pi.getId(), "End Process");
+
 	}
 
 }
