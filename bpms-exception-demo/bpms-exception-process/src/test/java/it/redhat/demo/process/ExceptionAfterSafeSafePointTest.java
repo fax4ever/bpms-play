3a877fb9719b7c57a4175a5823884af40af45f57
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-10-17 16:15:56 +0200

    adding more processes

---
+++ bpms-exception-demo/bpms-exception-process/src/test/java/it/redhat/demo/process/ExceptionAfterSafeSafePointTest.java
@@ -0,0 +1,66 @@
+package it.redhat.demo.process;
+
+import org.jbpm.test.JbpmJUnitBaseTestCase;
+import org.jbpm.workflow.instance.WorkflowRuntimeException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+
+import it.redhat.demo.RuntimeExceptionWIH;
+import stub.RESTWorkItemHandlerStub;
+
+public class ExceptionAfterSafeSafePointTest extends JbpmJUnitBaseTestCase {
+	
+	private static final String IT_REDHAT_DEMO = "it/redhat/demo/";
+	
+	private RuntimeManager runtimeManager;
+	private RuntimeEngine runtimeEngine;
+
+	private KieSession kieSession;
+	
+	public ExceptionAfterSafeSafePointTest() {
+		super(true, true);
+	}
+
+	@Before
+	public void before() {
+		runtimeManager = createRuntimeManager(IT_REDHAT_DEMO + "exception-after-safe-safe-point.bpmn2");
+		runtimeEngine = getRuntimeEngine();
+
+		kieSession = runtimeEngine.getKieSession();
+		this.kieSession.getWorkItemManager().registerWorkItemHandler("Rest", new RESTWorkItemHandlerStub());
+		kieSession.getWorkItemManager().registerWorkItemHandler("RuntimeException", new RuntimeExceptionWIH());
+	}
+
+	@After
+	public void after() {
+
+		runtimeManager.disposeRuntimeEngine(runtimeEngine);
+		runtimeManager.close();
+
+	}
+	
+	@Test
+	public void test() {
+		
+		Throwable cause = null;
+		
+		try {
+		
+			kieSession.startProcess("it.redhat.demo.exception-after-safe-safe-point");
+			
+		} catch (WorkflowRuntimeException e) {
+			
+			cause = e.getCause();
+			
+		}
+		
+		assertNotNull(cause);
+		assertEquals(WorkflowRuntimeException.class, cause.getClass());
+		
+	}
+
+}
