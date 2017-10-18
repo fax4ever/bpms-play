5828971006fa3ecc7aa342982f636cb739556ffe
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-10-18 15:34:33 +0200

    add test

--- bpms-exception-demo/bpms-exception-service/src/main/java/rest/RestServer.java
+++ bpms-exception-demo/bpms-exception-service/src/main/java/rest/RestServer.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by fabio.ercoli@redhat.com on 14/01/17.
  */
-@Path("")
+@Path("/")
 public class RestServer {
 
     private static Logger log = LoggerFactory.getLogger(RestServer.class);
@@ -20,39 +20,39 @@ public class RestServer {
         return "ciao";
     }
     
-    @Path("1")
+    @Path("/1")
     @GET
     public String ciao1() {
         log.info("ciao 1 invoked");
         return "ciao 1";
     }
     
-    @Path("2")
+    @Path("/2")
     @GET
     public String ciao2() {
         log.info("ciao 2 invoked");
         return "ciao 2";
     }
     
-    @Path("3")
+    @Path("/3")
     @GET
     public String ciao3() {
         log.info("ciao 3 invoked");
         return "ciao 3";
     }
     
-    @Path("slow")
+    @Path("/slow")
     @GET
     public String ciaoSlow() throws InterruptedException {
         log.info("ciao slow start");
         
         Thread.sleep(10000l);
         
-        log.info("ciao 3 slow end");
+        log.info("ciao slow end");
         return "ciao slow";
     }
     
-    @Path("error")
+    @Path("/error")
     @GET
     public String ciaoError() {
     	
