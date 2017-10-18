bfb005494be35e10f81ac78d31fdea82a7a99809
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-10-18 09:42:50 +0200

    add more exception cases

---
+++ bpms-exception-demo/bpms-exception-process/src/test/java/it/redhat/demo/process/TimeoutCatchRemoteTest.java
@@ -0,0 +1,65 @@
+package it.redhat.demo.process;
+
+import org.jbpm.bpmn2.handler.WorkItemHandlerRuntimeException;
+import org.jbpm.test.JbpmJUnitBaseTestCase;
+import org.jbpm.workflow.instance.WorkflowRuntimeException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeManager;
+
+import stub.WIRuntimeExceptionThrowerWid;
+
+public class TimeoutCatchRemoteTest extends JbpmJUnitBaseTestCase {
+	
+	private static final String IT_REDHAT_DEMO = "it/redhat/demo/";
+	
+	private RuntimeManager runtimeManager;
+	private RuntimeEngine runtimeEngine;
+
+	private KieSession kieSession;
+	
+	public TimeoutCatchRemoteTest() {
+		super(true, true);
+	}
+
+	@Before
+	public void before() {
+		runtimeManager = createRuntimeManager(IT_REDHAT_DEMO + "timeout-catch-remote-service.bpmn2");
+		runtimeEngine = getRuntimeEngine();
+
+		kieSession = runtimeEngine.getKieSession();
+		kieSession.getWorkItemManager().registerWorkItemHandler("Rest", new WIRuntimeExceptionThrowerWid());
+	}
+
+	@After
+	public void after() {
+
+		runtimeManager.disposeRuntimeEngine(runtimeEngine);
+		runtimeManager.close();
+
+	}
+	
+	@Test
+	public void test() {
+		
+		Throwable cause = null;
+		
+		try {
+		
+			kieSession.startProcess("it.redhat.demo.timeout-catch-remote-service");
+			
+		} catch (WorkflowRuntimeException e) {
+			
+			cause = e.getCause();
+			
+		}
+		
+		assertNotNull(cause);
+		assertEquals(WorkItemHandlerRuntimeException.class, cause.getClass());
+		
+	}
+
+}
