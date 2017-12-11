6739ab875cfa1cd3ac279d86f7b2c3a038891800
fax4ever <fabiomassimo.ercoli@gmail.com>
2017-12-11 12:56:28 +0100

    Red Hat Workshop BPMS

--- bpms-workshop/src/test/java/it/workshop/RivalseTest.java
+++ bpms-workshop/src/test/java/it/workshop/RivalseTest.java
@@ -1,5 +1,8 @@
 package it.workshop;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.jbpm.test.JbpmJUnitBaseTestCase;
 import org.junit.After;
 import org.junit.Before;
@@ -7,7 +10,9 @@ import org.junit.Test;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.manager.RuntimeEngine;
 import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
 
 public class RivalseTest extends JbpmJUnitBaseTestCase {
 	
@@ -41,7 +46,54 @@ public class RivalseTest extends JbpmJUnitBaseTestCase {
 	}
 	
 	@Test
-	public void test() {
+	public void test_revoca() {
+		
+		HashMap<String, Object> paramters = new HashMap<>();
+		paramters.put("idPratica", "ID1234567");
+		
+		ProcessInstance processInstance = kieSession.startProcess("it.workshop.rivalse", paramters);
+		
+		assertProcessInstanceActive(processInstance.getId());
+		assertNodeTriggered(processInstance.getId(), "StartProcess", "Presa In Carico");
+		
+		kieSession.signalEvent("revoca", "Pratica non lavorabile. Codice 232.");
+		
+		assertProcessInstanceAborted(processInstance.getId());
+		assertNodeTriggered(processInstance.getId(), "Revoca Pratica", "Start Revoca", "End Revoca");
+		
+	}
+	
+	@Test
+	public void test_rifiuto() {
+		
+		HashMap<String, Object> paramters = new HashMap<>();
+		paramters.put("idPratica", "ID1234567");
+		
+		ProcessInstance processInstance = kieSession.startProcess("it.workshop.rivalse", paramters);
+		
+		assertProcessInstanceActive(processInstance.getId());
+		assertNodeTriggered(processInstance.getId(), "StartProcess", "Presa In Carico");
+		
+		List<TaskSummary> marcoTasks = taskService.getTasksAssignedAsPotentialOwner("marco", null);
+		assertEquals(1, marcoTasks.size());
+		
+		TaskSummary task = marcoTasks.get(0);
+		
+		taskService.claim(task.getId(), "marco");
+		taskService.start(task.getId(), "marco");
+		
+		paramters.clear();
+		paramters.put("accettazione", false);
+		
+		taskService.complete(task.getId(), "marco", paramters);
+		
+		assertProcessInstanceActive(processInstance.getId());
+		assertNodeTriggered(processInstance.getId(), "accettazione / rifiuto", "Conferma Rifiuto");
+		
+		kieSession.signalEvent("rifiuto", "Rifiuto accordato. Codice 121.");
+		
+		assertProcessInstanceCompleted(processInstance.getId());
+		assertNodeTriggered(processInstance.getId(), "End Rifiuto");
 		
 	}
 
